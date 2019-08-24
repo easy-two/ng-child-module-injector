37bf96e6e9b4abff5c2dbe86dbf0f8c6dbafc81b
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2019-08-24 16:46:28 +0300

    feat(*): add custom injector module and render initial component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Injector,
+  NgModuleFactory,
+  NgModuleFactoryLoader,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import { WithCustomInjectorComponent } from './with-custom-injector/with-custom-injector/with-custom-injector.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ng-custom-injector-modules';
+
+  @ViewChild('testOutlet', {read: ViewContainerRef}) testOutlet: ViewContainerRef;
+  constructor(
+    private loader: NgModuleFactoryLoader,
+    private injector: Injector
+  ) {}
+
+  ngAfterViewInit(): void {
+    const path = 'src/app/with-custom-injector/with-custom-injector.module#WithCustomInjectorModule';
+    this.loader.load(path).then((moduleFactory: NgModuleFactory<any>) => {
+      const moduleRef = moduleFactory.create(this.injector);
+      const compFactory = moduleRef.componentFactoryResolver.resolveComponentFactory(WithCustomInjectorComponent);
+      this.testOutlet.createComponent(compFactory);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NgModuleFactoryLoader, SystemJsNgModuleLoader } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
